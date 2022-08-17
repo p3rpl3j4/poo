49cacf528d7980d6f95a795e4011a972f6af8738
p3rpl3j4 <lucy.marticorena@gmail.com>
2022-08-17 11:22:39 -0300

    Ejemplo con herencia

--- Zoo/src/Gato.java
+++ Zoo/src/Gato.java
@@ -1,10 +1,10 @@
-public class Gato {
+public class Gato extends Animal{
 	/* Atributos */
-	String nombre;
-	int edad;
-	String genero;
-	float peso;
-	String raza;
+	private String nombre;
+	private int edad;
+	private String genero;
+	private float peso;
+	private String raza;
 
 	
 	/* Constructores de la clase */
@@ -28,11 +28,7 @@ public class Gato {
 	
 	
 	/* Métodos */
-	public void caminar() {
-		System.out.println("Falta implementar método caminar");
-	}
-	
-	public void comer() {
+		public void comer() {
 		System.out.println("Falta implementar método comer");
 	}
 	
@@ -63,5 +59,45 @@ public class Gato {
 	  */
 	public int estaEsMiEdad() {
 		return this.edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
 	}
 }
--- Zoo/src/Perro.java
+++ Zoo/src/Perro.java
@@ -1,13 +1,15 @@
-public class Perro {
+public class Perro extends Animal{
 	/* Atributos */
-	String nombre;
-	int edad;
-	String genero;
-	float peso;
-	String raza;
+	private String nombre;
+	private int edad;
+	private String genero;
+	private float peso;
+	private String raza;
 
 	
 	/* Constructores de la clase */
+	//puedo tener más de un constructor. Es buena práctica que el constructor
+	//se encargue de inicializar todos los atributos
 	public Perro() {
 		// TODO Auto-generated constructor stub
 	}
@@ -28,7 +30,7 @@ public class Perro {
 	
 	/* Métodos */
 	public void caminar() {
-		System.out.println("Falta implementar método caminar");
+		System.out.println("Soy un perro y puedo caminar");
 	}
 	
 	public void comer() {
@@ -45,5 +47,45 @@ public class Perro {
 	
 	public int estaEsMiEdad() {
 		return this.edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
 	}
 }
